2f44bee0d4f79fa4f85d2e43b7817cb3cfce42d6
yuwatanabe <yuwatanabe9@gmail.com>
2021-07-10 19:19:47 +0900

    about JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React, { Component } from "react";
 
-function App() {
-  return <>Hello</>
+class App extends Component {
+  render(){
+    // const greeting = "Hi!";
+    // const dom = <h2>{greeting}</h2>;
+    // return dom;
+   
+    // return <input type="text" onClick={()=>{console.log("Im Happy")}}></input>
+
+    return(
+      <div>
+        <label htmlForm="bar">bar</label>
+        <input type="text" onChange={()=>{console.log("Im Happy")}}></input>
+      </div>
+    )
+  }
 }
 
 export default App;
