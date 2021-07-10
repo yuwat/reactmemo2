3e61f19e34375013cd951a086af7e1153524ca5e
yuwat <>
2021-07-10 19:53:10 +0900

    component test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render(){
-    // const greeting = "Hi!";
-    // const dom = <h2>{greeting}</h2>;
-    // return dom;
+// class App extends Component {
+//   render(){
+//     // const greeting = "Hi!";
+//     // const dom = <h2>{greeting}</h2>;
+//     // return dom;
    
-    // return <input type="text" onClick={()=>{console.log("Im Happy")}}></input>
+//     // return <input type="text" onClick={()=>{console.log("Im Happy")}}></input>
 
-    return(
-      <div>
-        <label htmlForm="bar">bar</label>
-        <input type="text" onChange={()=>{console.log("Im Happy")}}></input>
-      </div>
-    )
-  }
+//     return(
+//       <div>
+//         <label htmlForm="bar">bar</label>
+//         <input type="text" onChange={()=>{console.log("Im Happy")}}></input>
+//       </div>
+//     )
+//   }
+// }
+const App = () => {
+  return (
+    <div>
+      <Cat />
+    </div>
+  )
 }
+const Cat = () => {
+  return (
+    <div>Hi!</div>
+  )
+}
+
+
+
 
 export default App;
